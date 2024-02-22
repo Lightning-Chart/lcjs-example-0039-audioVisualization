3542cb703fc3e73c2b925dc7a77e4d597a043d1a
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2024-02-22 14:22:19 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,72 @@
+# JavaScript Real-Time MP3 Visualization
+
+![JavaScript Real-Time MP3 Visualization](audioVisualization-darkGold.png)
+
+This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
+
+LightningChart JS is entirely GPU accelerated and performance optimized charting library for presenting massive amounts of data. It offers an easy way of creating sophisticated and interactive charts and adding them to your website or web application.
+
+The demo can be used as an example or a seed project. Local execution requires the following steps:
+
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
+
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
+
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
+
+
+## Description
+
+Example of using LightningChart JS to visualize a MP3 audio file in real-time.
+
+---
+
+Noise by Alex-Productions | https://onsound.eu/
+
+Music promoted by https://www.free-stock-music.com
+
+Creative Commons / Attribution 3.0 Unported License (CC BY 3.0)
+
+https://creativecommons.org/licenses/by/3.0/deed.en_US
+
+
+## API Links
+
+* [LightningChart]
+* [Automatic tick placement strategies]
+* [Chart XY]
+* [Axis XY]
+* [Line Series XY]
+* [Legend Box]
+
+
+## Support
+
+If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
+
+Official [API documentation][1] can be found on [LightningChart][2] website.
+
+If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
+
+If you think you found a bug in the LightningChart JavaScript library, please contact sales@lightningchart.com.
+
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
+
+[0]: https://github.com/Arction/
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
+[3]: https://stackoverflow.com/questions/tagged/lightningchart
+[4]: https://lightningchart.com/support-services/
+
+© LightningChart Ltd 2009-2022. All rights reserved.
+
+
+[LightningChart]: https://lightningchart.com/js-charts/api-documentation/v5.1.0/functions/lightningChart-1.html
+[Automatic tick placement strategies]: https://lightningchart.com/js-charts/api-documentation/v5.1.0/variables/AxisTickStrategies.html
+[Chart XY]: https://lightningchart.com/js-charts/api-documentation/v5.1.0/classes/ChartXY.html
+[Axis XY]: https://lightningchart.com/js-charts/api-documentation/v5.1.0/classes/Axis.html
+[Line Series XY]: https://lightningchart.com/js-charts/api-documentation/v5.1.0/classes/LineSeries.html
+[Legend Box]: https://lightningchart.com/js-charts/api-documentation/v5.1.0/classes/Chart.html#addLegendBox
+
---
+++ src/index.js
@@ -0,0 +1,134 @@
+const lcjs = require('@arction/lcjs')
+const { lightningChart, Themes, emptyFill, PalettedFill, LUT, AxisTickStrategies, AxisScrollStrategies, emptyLine, regularColorSteps } =
+    lcjs
+
+const exampleContainer = document.getElementById('chart') || document.body
+if (exampleContainer === document.body) {
+    exampleContainer.style.width = '100vw'
+    exampleContainer.style.height = '100vh'
+    exampleContainer.style.margin = '0px'
+}
+
+const lc = lightningChart()
+
+const temporaryPanelAtStart = lc.UIPanel({
+    container: exampleContainer,
+    // theme: Themes.darkGold
+})
+const ui = temporaryPanelAtStart.addUIElement().setPadding(12).setText('Click anywhere to start playing audio').setMouseInteractions(false)
+temporaryPanelAtStart.onBackgroundMouseClick((_) => startAudioVisualization())
+const theme = temporaryPanelAtStart.getTheme()
+
+const startAudioVisualization = () => {
+    temporaryPanelAtStart.dispose()
+    const containerChart1 = document.createElement('div')
+    const containerChart2 = document.createElement('div')
+    exampleContainer.append(containerChart1)
+    exampleContainer.append(containerChart2)
+    containerChart1.style.width = '100%'
+    containerChart1.style.height = '50%'
+    containerChart2.style.width = '100%'
+    containerChart2.style.height = '50%'
+
+    // Loosely based on: https://blog.logrocket.com/audio-visualizer-from-scratch-javascript/
+    let audio1 = new Audio()
+    audio1.src = document.head.baseURI + 'examples/assets/0039/alex-productions-noise.mp3'
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+    let audioSource = null
+    let analyser = null
+    audioSource = audioCtx.createMediaElementSource(audio1)
+    analyser = audioCtx.createAnalyser()
+    audioSource.connect(analyser)
+    analyser.connect(audioCtx.destination)
+    analyser.fftSize = 128
+    audio1.play()
+    const bufferLength = analyser.frequencyBinCount
+
+    // (1) Frequency visualization
+    const chartFrequency = lc
+        .ChartXY({
+            container: containerChart1,
+        })
+        .setTitle('Alex-Productions - Noise')
+        .setTitlePosition('series-right-top')
+        .setPadding(30)
+        .setMouseInteractions(false)
+    const palette = new PalettedFill({
+        lookUpProperty: 'y',
+        lut: new LUT({
+            interpolate: true,
+            steps: regularColorSteps(0, 200, theme.examples.spectrogramColorPalette),
+        }),
+    })
+    const seriesFrequency = chartFrequency
+        .addPointLineAreaSeries({ dataPattern: 'ProgressiveX' })
+        .setCurvePreprocessing({ type: 'spline', resolution: 10 })
+        .setPointFillStyle(emptyFill)
+        .setAreaFillStyle(palette)
+        .setCursorEnabled(false)
+    const axisY = chartFrequency.getDefaultAxisY().setInterval({ start: 0, end: 256 })
+    chartFrequency.forEachAxis((axis) => axis.setTickStrategy(AxisTickStrategies.Empty).setMouseInteractions(false))
+
+    // (2) Waveform
+    const chartWaveform = lc
+        .ChartXY({
+            container: containerChart2,
+        })
+        .setTitle('')
+        .setPadding(30)
+    const seriesWaveform = chartWaveform
+        .addPointLineAreaSeries({
+            dataPattern: 'ProgressiveX',
+            allowInputModification: false,
+        })
+        .setMaxSampleCount(100_000)
+        .setPointFillStyle(emptyFill)
+        .setAreaFillStyle(emptyLine)
+        .setCursorEnabled(false)
+    chartWaveform
+        .getDefaultAxisX()
+        .setScrollStrategy(AxisScrollStrategies.progressive)
+        .setTickStrategy(AxisTickStrategies.Time)
+        .setDefaultInterval((state) => ({
+            start: (state.dataMax ?? 0) - 5_000,
+            end: state.dataMax,
+            stopAxisAfter: false,
+        }))
+        .fit(false)
+    chartWaveform.getDefaultAxisY().setAnimationScroll(false).setChartInteractionZoomByWheel(false)
+    chartWaveform.getDefaultAxisY().setTickStrategy(AxisTickStrategies.Empty)
+    chartWaveform.getDefaultAxisX().setTickStrategy(AxisTickStrategies.Time)
+
+    //
+    const dataArrayFrequency = new Uint8Array(bufferLength)
+    const dataArrayTimeDomain = new Float32Array(analyser.fftSize)
+    let tPrev = undefined
+    const tFirst = performance.now()
+    let framesCount = 0
+    const title = chartFrequency.getTitle()
+    const frame = () => {
+        const tNow = performance.now()
+        if (tPrev) {
+            analyser.getByteFrequencyData(dataArrayFrequency)
+            analyser.getFloatTimeDomainData(dataArrayTimeDomain)
+            seriesFrequency.clear().appendSamples({
+                yValues: dataArrayFrequency,
+            })
+            const waveformTimestamps = new Array(dataArrayTimeDomain.length)
+                .fill(0)
+                .map((_, i, arr) => tPrev + ((tNow - tPrev) * (i + 1)) / arr.length)
+            seriesWaveform.appendSamples({
+                xValues: waveformTimestamps,
+                yValues: dataArrayTimeDomain,
+            })
+            chartWaveform.getDefaultAxisX().setStopped(false)
+
+            const fps = 1000 / ((tNow - tFirst) / framesCount)
+            chartFrequency.setTitle(`${title} - ${fps.toFixed(0)} FPS`)
+        }
+        requestAnimationFrame(frame)
+        tPrev = tNow
+        framesCount += 1
+    }
+    frame()
+}
